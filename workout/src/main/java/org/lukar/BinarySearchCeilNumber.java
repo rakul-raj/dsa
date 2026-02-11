189064ff5a5fbe1b29ba063d1ebc8cb2a65e83cf
rakul-raj <rakulm.raj@gmail.com>
2026-02-11 20:53:36 +0530

    BS Ceil numbers

---
+++ workout/src/main/java/org/lukar/BinarySearchCeilNumber.java
@@ -0,0 +1,38 @@
+package org.lukar;
+
+public class BinarySearchCeilNumber {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 3, 4, 5, 8, 11, 34, 65, 88, 91, 99};
+
+        int target = 89;
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        int ansIndex= findNumber(arr,target,start,end);
+
+        System.out.println(arr[ansIndex]);
+
+
+    }
+
+
+    static int findNumber(int[] arr, int target, int start, int end){
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+
+            } else {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
